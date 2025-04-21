85aa16cb4adfb22df1a3c66cd08fec0d3a874d3f
Zaneta Sochon <twj email>
2025-04-21 16:17:16 +0200

    feat(01): Find element include data-info atribute

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,18 @@
-console.log('DOM');
+const commentsItemNewest = document.querySelector('.comments__item', '.comments__item--newest');
+const childrenOfLi = commentsItemNewest.querySelectorAll('p');
+
+let listOfSpan;
+const spanArr = [];
+
+const listOfP = childrenOfLi.forEach((el) => {
+  listOfSpan = el.querySelectorAll('span');
+
+  for (let i = 0; i < listOfSpan.length; i++) {
+    spanArr.push(listOfSpan[i]);
+  }
+});
+
+console.log(
+  'Zostało znalezionych elementów posiadających atrybut data-info w liczbie:',
+  spanArr.length
+);
