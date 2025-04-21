1b0e0da4463a19856474fc15ee92285c39f83754
Zaneta Sochon <twj email>
2025-04-21 17:13:20 +0200

    feat(02): Add href atributes to dataset elements

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,11 @@
-console.log('DOM');
+const aList = document.querySelectorAll('a');
+console.log(aList);
+
+let hasAttribute;
+
+const hasLink = aList.forEach((link) => {
+  if (link.hasAttribute('data-url')) {
+    hasAttribute = link.getAttribute('data-url');
+    link.setAttribute('href', hasAttribute);
+  }
+});
