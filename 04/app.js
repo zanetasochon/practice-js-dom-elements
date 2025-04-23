d440a7163d6b1d1bf8949fb0ebd3f226aef71913
Zaneta Sochon <twj email>
2025-04-23 21:30:30 +0200

    feat(04): Create unordered list with links

--- 04/app.js
+++ 04/app.js
@@ -2,7 +2,27 @@ console.log('DOM');
 
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+  { text: 'start', url: '/' },
+  { text: 'galeria', url: '/gallery' },
+  { text: 'kontakt', url: '/contact' },
 ];
+
+const navEl = document.querySelector('nav');
+
+const ulEl = document.createElement('ul');
+navEl.appendChild(ulEl);
+
+const arr = [];
+
+const createLinkList = menuItems.forEach((item) => {
+  const liStartEl = document.createElement('li');
+  ulEl.appendChild(liStartEl);
+
+  arr.push(liStartEl);
+
+  const addAEl = document.createElement('a');
+  liStartEl.appendChild(addAEl);
+
+  addAEl.setAttribute('href', item.url);
+  addAEl.textContent = item.text;
+});
