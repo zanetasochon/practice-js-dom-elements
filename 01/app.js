e0165681cb12720e938f9ce01e4aefffb7e64f23
Zaneta Sochon <twj email>
2025-04-26 19:15:20 +0200

    fix: Read span elements number

--- 01/app.js
+++ 01/app.js
@@ -1,18 +1,7 @@
 const commentsItemNewest = document.querySelector('.comments__item.comments__item--newest');
-const childrenOfLi = commentsItemNewest.querySelectorAll('p');
-
-let listOfSpan;
-const spanArr = [];
-
-const listOfP = childrenOfLi.forEach((el) => {
-  listOfSpan = el.querySelectorAll('span');
-
-  for (let i = 0; i < listOfSpan.length; i++) {
-    spanArr.push(listOfSpan[i]);
-  }
-});
+const childrenOfP = commentsItemNewest.querySelectorAll('span');
 
 console.log(
   'Zostało znalezionych elementów posiadających atrybut data-info w liczbie:',
-  spanArr.length
+  childrenOfP.length
 );
