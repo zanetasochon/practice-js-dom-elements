f242cc252ad7df877369f88ca6b8dd9f375ebbf3
Zaneta Sochon <twj email>
2025-04-26 19:23:40 +0200

    fix: Get only a[data-url] selectiors

--- 02/app.js
+++ 02/app.js
@@ -1,11 +1,8 @@
-const aList = document.querySelectorAll('a');
-console.log(aList);
+const aList = document.querySelectorAll('a[data-url]');
 
 let hasAttribute;
 
 const hasLink = aList.forEach((link) => {
-  if (link.hasAttribute('data-url')) {
-    hasAttribute = link.getAttribute('data-url');
-    link.setAttribute('href', hasAttribute);
-  }
+  hasAttribute = link.getAttribute('data-url');
+  link.setAttribute('href', hasAttribute);
 });
